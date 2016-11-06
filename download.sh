df427b2c2e09d66da415d5d9c4d40bd6924b5a5a
brad thompson <brad@lambda-dynamics.com>
2016-11-06 11:40:58 -0500

    rebase download to master for now

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://bitbucket.org/lambdadynamics/eztv/get/1.0.tar.gz -O eztv.tar.gz
+wget https://bitbucket.org/lambdadynamics/eztv/get/master.tar.gz -O eztv.tar.gz
 tar -xf eztv.tar.gz
 mv lambdadynamics-eztv-* EZTV
 rm eztv.tar.gz
